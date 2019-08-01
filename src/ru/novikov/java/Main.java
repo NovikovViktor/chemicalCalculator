a2d7dfb8966906009b88102586f10b07954454e2
Viktor <48834432+OldFrostSnake@users.noreply.github.com>
2019-08-01 21:17:58 +0300

    almost finish design, and set controller

--- src/ru/novikov/java/Main.java
+++ src/ru/novikov/java/Main.java
@@ -5,14 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+    private double xOffset;
+    private double yOffset;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/ru/novikov/resources/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        root.setOnMousePressed((event) ->{
+                xOffset = primaryStage.getX() - event.getScreenX();
+                yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        root.setOnMouseDragged((event) -> {
+                primaryStage.setX(event.getScreenX() + xOffset);
+                primaryStage.setY(event.getScreenY() + yOffset);
+        });
+        primaryStage.setTitle("Chemical calculator");
+        primaryStage.setScene(new Scene(root, 496, 374));
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
--- src/ru/novikov/java/controller/Controller.java
+++ src/ru/novikov/java/controller/Controller.java
@@ -1,4 +1,35 @@
 package ru.novikov.java.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+    @FXML
+    private TextField mBlud, mChash, mChashAndObr, procOpen, drySubs, mDry,
+            calcDrySubs, zola, aminCarbo, jMd, pJ, sMd, aminUmd, kmd, sF, jF, aminUf, kF;
+    @FXML
+    private Button firstCalc, secondCalc, thirdCalc, clear;
+
+    @FXML
+    public void firstCalcAct(ActionEvent event){
+        //mBlud.getText();
+    }
+    @FXML
+    public void secondCalcAct(ActionEvent event){
+
+    }
+    @FXML
+    public void thirdCalcAct(ActionEvent event){
+
+    }
+    @FXML
+    public void clearAct(ActionEvent event){
+
+    }
 }
